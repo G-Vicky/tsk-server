b8bf05c295d238a17f9837c227b8b30266f01064
G-Vicky <vigneshwaran2747@gmail.com>
2021-05-30 15:44:16 +0530

    modified header informations

--- app.js
+++ app.js
@@ -26,8 +26,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token"
   );
+  res.header("Access-Control-Expose-Headers", "x-auth-token");
+  res.header("Access-Control-Expose-Headers", "Authorization");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
     return res.status(200).json({});
