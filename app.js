466d55acfbeaab9e0fb761acec611f4bca5d5863
G-Vicky <vigneshwaran2747@gmail.com>
2021-05-30 15:54:13 +0530

    exposed all headers

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token"
   );
-  res.header("Access-Control-Expose-Headers", "x-auth-token");
-  res.header("Access-Control-Expose-Headers", "Authorization");
+  // res.header("Access-Control-Expose-Headers", "x-auth-token");
+  res.header("Access-Control-Expose-Headers", "*, Authorization");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
     return res.status(200).json({});
